85dd4cd94b2cd413671cea48d4cded84f4c0d8f8
JaySinghX <jaysinghtalreja@gmail.com>
2020-02-14 20:00:48 +0530

    Back Button Added on the About Screen

--- app/src/main/java/com/example/incrypter/About.java
+++ app/src/main/java/com/example/incrypter/About.java
@@ -1,8 +1,10 @@
 package com.example.incrypter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class About extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        //Setting Back Button in Custom Action Bar On File Action Activity
+        Toolbar toolbar = (Toolbar) findViewById(R.id.aboutactionbar);
+        setSupportActionBar(toolbar);
+        //Set Back Button
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        //No Title On Action Bar
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+
+    //Explicit Back Button Task on About Section
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
